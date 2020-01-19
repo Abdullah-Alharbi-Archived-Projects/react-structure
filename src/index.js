6d123758571722798bdc3e10b5e2de4bdf6ca53f
W2AlharbiMe <w2.alharbi.me@Gmail.com>
2020-01-19 08:31:40 +0300

    basic structure

--- src/index.js
+++ src/index.js
@@ -1,10 +1,29 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import { createStore, applyMiddleware } from "redux";
+import reduxSagaMiddleware from "redux-saga";
+import { BrowserRouter } from "react-router-dom";
+import { Provider } from "react-redux";
+import App from "./App";
+import * as serviceWorker from "./serviceWorker";
+import "./index.css";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const sagaMiddleware = reduxSagaMiddleware();
+// TODO: compine reducers
+const store = createStore((state = {}, action) => {},
+applyMiddleware(sagaMiddleware));
+
+// TODO: sagas goes here
+// sagaMiddleware.run();
+
+ReactDOM.render(
+  <Provider store={store}>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </Provider>,
+  document.getElementById("root")
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
